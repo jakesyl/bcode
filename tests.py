1e9a91b70b9d835fb85fa27e9ab0bf9776e2431c
Pedro Rodrigues <medecau@gmail.com>
2013-10-23 17:25:42 +0100

    fixing tests

--- tests.py
+++ tests.py
@@ -14,13 +14,13 @@ class TestEncoding(unittest.TestCase):
 
     # STRINGS
     def test_encode_string(self):
-        self.assertEqual(bencode('spam'), '4: spam')
+        self.assertEqual(bencode('spam'), '4:spam')
 
     def test_encode_unicode_string_1(self):
-        self.assertEqual(bencode(u'eggs'), '4: eggs')
+        self.assertEqual(bencode(u'eggs'), '4:eggs')
 
     def test_encode_unicode_string_2(self):
-        self.assertEqual(bencode(u'pão'), '4: p\xc3\xa3o')
+        self.assertEqual(bencode(u'pão'), '4:p\xc3\xa3o')
 
     # INTEGERS
     def test_encode_integer(self):
@@ -28,11 +28,11 @@ class TestEncoding(unittest.TestCase):
 
     # FLOATS
     def test_encode_float(self):
-        self.assertEqual(bencode(3.3), '3: 3.3')
+        self.assertEqual(bencode(3.3), '3:3.3')
 
     # ITERABLES
     def test_encode_list(self):
-        self.assertEqual(bencode(['spam', 'eggs']), 'l4: spam4: eggse')
+        self.assertEqual(bencode(['spam', 'eggs']), 'l4:spam4:eggse')
 
     def test_encode_tuple(self):
         self.assertEqual(bencode((1, 2)), 'li1ei2ee')
@@ -41,19 +41,19 @@ class TestEncoding(unittest.TestCase):
         self.assertEqual(bencode(xrange(1, 6)), 'li1ei2ei3ei4ei5ee')
 
     def test_encode_list_in_list(self):
-        self.assertEqual(bencode(['spam', ['a', 'b']]), 'l4: spaml1: a1: bee')
+        self.assertEqual(bencode(['spam', ['a', 'b']]), 'l4:spaml1:a1:bee')
 
     def test_encode_dict_two_members(self):
         self.assertEqual(bencode({'cow': 'moo', 'spam': 'eggs'}),
-                         'd3: cow3: moo4: spam4: eggse')
+                         'd3:cow3:moo4:spam4:eggse')
 
     def test_encode_list_in_dict(self):
-        self.assertEqual(bencode({'spam': ['a', 'b']}), 'd4: spaml1: a1: bee')
+        self.assertEqual(bencode({'spam': ['a', 'b']}), 'd4:spaml1:a1:bee')
 
     def test_encode_dict_in_dict(self):
         self.assertEqual(bencode({'fruit': {'a': 'apple', 'b': 'banana'},
                                   'cow': 'moo'}),
-                         'd5: fruitd1: a5: apple1: b6: bananae3: cow3: mooe')
+                         'd5:fruitd1:a5:apple1:b6:bananae3:cow3:mooe')
 
 
 class TestDecoding(unittest.TestCase):
@@ -66,46 +66,46 @@ class TestDecoding(unittest.TestCase):
 
     # FLOATS
     def test_decode_single_float(self):
-        self.assertEqual(bdecode('3: 3.3'), '3.3')
+        self.assertEqual(bdecode('3:3.3'), '3.3')
 
     # STRINGS
     def test_decode_single_string_1(self):
-        self.assertEqual(bdecode('4: spam'), 'spam')
+        self.assertEqual(bdecode('4:spam'), 'spam')
 
     def test_decode_single_string_2(self):
-        self.assertEqual(bdecode('13: spam and eggs'), 'spam and eggs')
+        self.assertEqual(bdecode('13:spam and eggs'), 'spam and eggs')
 
     def test_decode_single_unicode_string(self):
-        self.assertEqual('p\xe3o', bdecode('6: p\xe3o'))
+        self.assertEqual('p\xe3o', bdecode('6:p\xe3o'))
 
     def test_decode_short_string(self):
-        self.assertEqual('egg', bdecode('4: egg'))
+        self.assertEqual('egg', bdecode('4:egg'))
 
     # LISTS
     def test_decode_single_list(self):
-        self.assertEqual(bdecode('l4: spam4: eggse'), ['spam', 'eggs'])
+        self.assertEqual(bdecode('l4:spam4:eggse'), ['spam', 'eggs'])
 
     def test_decode_list_in_list(self):
-        self.assertEqual(bdecode('l4: spaml1: a1: bee'), ['spam', ['a', 'b']])
+        self.assertEqual(bdecode('l4:spaml1:a1:bee'), ['spam', ['a', 'b']])
 
     def test_decode_dicts_in_list(self):
-        self.assertEqual(bdecode('ld1: ai1e1: bi2eed1: ci3e1: di4eee'),
+        self.assertEqual(bdecode('ld1:ai1e1:bi2eed1:ci3e1:di4eee'),
                          [{'a': 1, 'b': 2}, {'c': 3, 'd': 4}])
 
     # DICTS
     def test_decode_single_dict(self):
-        self.assertEqual(bdecode('d3: cow3: moo4: spam4: eggse'),
+        self.assertEqual(bdecode('d3:cow3:moo4:spam4:eggse'),
                          {'cow': 'moo', 'spam': 'eggs'})
 
     def test_decode_list_in_dict(self):
-        self.assertEqual(bdecode('d4: spaml1: a1: bee'), {'spam': ['a', 'b']} )
+        self.assertEqual(bdecode('d4:spaml1:a1:bee'), {'spam': ['a', 'b']} )
 
     def test_decode_dict_in_dict(self):
-        self.assertEqual(bdecode('d4: spamd1: a1: bee'), {'spam': {'a': 'b'}})
+        self.assertEqual(bdecode('d4:spamd1:a1:bee'), {'spam': {'a': 'b'}})
 
     # MULTIPLE TYPES
     def test_decode_complex_1(self):
-        self.assertEqual(bdecode('d4: teamld4: name3: bob3: agei30e6: skillsl6: python4: htmleed4: name5: jimmy3: agei32e6: skillsleeee'), {'team': [{'skills': ['python', 'html'], 'age': 30, 'name': 'bob'}: {'skills': [], 'age': 32, 'name': 'jimmy'}]})
+        self.assertEqual(bdecode('d4:teamld4:name3:bob3:agei30e6:skillsl6:python4:htmleed4:name5:jimmy3:agei32e6:skillsleeee'), {'team': [{'skills': ['python', 'html'], 'age': 30, 'name': 'bob'}, {'skills': [], 'age': 32, 'name': 'jimmy'}]})
 
 if __name__ == '__main__':
     unittest.main()
